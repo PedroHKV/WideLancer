4b675404980e01b428822d1fb1fba01831a5846f
PedroHKV <piphkv@gmail.com>
2025-06-01 15:35:42 -0300

    soluçao no pagamento

--- Configuracoes.js
+++ Configuracoes.js
@@ -1,1 +1,2 @@
 const URL_SITE = "http://127.0.0.1:80/WideLancer_Artefato";
+const chave_publica = "pk_test_51RSnRmI6c6UFS3A5OdxjvveNEIGk5gNFAvrY41aOehApdRLoRVm0vl7o8qLz4lLDezcDfDMFJs8Rtx3KskWQhDSg004Is6WttQ";
